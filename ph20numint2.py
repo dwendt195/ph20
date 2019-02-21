dfb95cb85a0b8704e49691a5ac6d09a5c32b5ff5
Danny Wendt <dwendt195@gmail.com>
2019-02-20 16:37:09 -0800

    Create .py version of ph20numint2 for use in makefile project

---
+++ ph20numint2.py
@@ -0,0 +1,142 @@
+import sys
+import numpy as np
+from numpy.linalg import inv
+import matplotlib.pyplot as plt
+
+#mass on a spring: d2x/dt2=-k/m x
+
+def euler(x0,v0,n,h,fig):
+    t = np.zeros(n)
+    x = np.zeros(n)
+    v = np.zeros(n)
+    x[0] = x0
+    v[0] = v0
+    for i in range (n-1):
+        t[i+1]=h*(i+1)
+        v[i+1]=v[i]-h*x[i]
+        x[i+1]=x[i]+h*v[i]
+    fig=plt.plot(t,x,label='Position (x)')
+    fig=plt.plot(t,v,label='Velocity (v)')
+    
+def eulererror(x0,v0,n,h,ax):
+    t = np.zeros(n)
+    x = np.zeros(n)
+    v = np.zeros(n)
+    x[0] = x0
+    v[0] = v0
+    for i in range (n-1):
+        t[i+1]=h*(i+1)
+        v[i+1]=v[i]-h*x[i]
+        x[i+1]=x[i]+h*v[i]
+    xerr=np.cos(t)-x
+    verr=-np.sin(t)-v
+    
+    ax[0].plot(t,xerr,label='h = '+str(h))
+    ax[1].plot(t,verr,label='h = '+str(h))
+    
+def eulerenergy(x0,v0,n,h,fig):
+    t = np.zeros(n)
+    x = np.zeros(n)
+    v = np.zeros(n)
+    x[0] = x0
+    v[0] = v0
+    for i in range (n-1):
+        t[i+1]=h*(i+1)
+        v[i+1]=v[i]-h*x[i]
+        x[i+1]=x[i]+h*v[i]
+    E = x*x+v*v
+    
+    plt.plot(t,E)
+    
+def eulerimplicit(x0,v0,n,h,fig):
+    t = np.zeros(n)
+    x = np.zeros(n)
+    v = np.zeros(n)
+    x[0] = x0
+    v[0] = v0
+    for i in range (n-1):
+        t[i+1]=h*(i+1)
+        v[i+1]=(v[i]-h*x[i])/(1+h*h)
+        x[i+1]=(x[i]+h*v[i])/(1+h*h)
+    fig=plt.plot(t,x,label='Position (x)')
+    fig=plt.plot(t,v,label='Velocity (v)')
+    
+def eulererrorimp(x0,v0,n,h,ax):
+    t = np.zeros(n)
+    x = np.zeros(n)
+    v = np.zeros(n)
+    x[0] = x0
+    v[0] = v0
+    for i in range (n-1):
+        t[i+1]=h*(i+1)
+        v[i+1]=(v[i]-h*x[i])/(1+h*h)
+        x[i+1]=(x[i]+h*v[i])/(1+h*h)
+    xerr=np.cos(t)-x
+    verr=-np.sin(t)-v
+    
+    ax[0].plot(t,xerr,label='h = '+str(h))
+    ax[1].plot(t,verr,label='h = '+str(h))
+    
+def eulerenergyimp(x0,v0,n,h,fig):
+    t = np.zeros(n)
+    x = np.zeros(n)
+    v = np.zeros(n)
+    x[0] = x0
+    v[0] = v0
+    for i in range (n-1):
+        t[i+1]=h*(i+1)
+        v[i+1]=(v[i]-h*x[i])/(1+h*h)
+        x[i+1]=(x[i]+h*v[i])/(1+h*h)
+    E = x*x+v*v
+    
+    plt.plot(t,E)
+    
+def eulerphaseexp(x0,v0,n,h,fig):
+    t = np.zeros(n)
+    x = np.zeros(n)
+    v = np.zeros(n)
+    x[0] = x0
+    v[0] = v0
+    for i in range (n-1):
+        t[i+1]=h*(i+1)
+        v[i+1]=v[i]-h*x[i]
+        x[i+1]=x[i]+h*v[i]
+    fig=plt.plot(x,v,label='Explicit')
+    fig=plt.plot(np.cos(t),-np.sin(t),label='Analytic')
+    
+def eulerphaseimp(x0,v0,n,h,fig):
+    t = np.zeros(n)
+    x = np.zeros(n)
+    v = np.zeros(n)
+    x[0] = x0
+    v[0] = v0
+    for i in range (n-1):
+        t[i+1]=h*(i+1)
+        v[i+1]=(v[i]-h*x[i])/(1+h*h)
+        x[i+1]=(x[i]+h*v[i])/(1+h*h)
+    fig=plt.plot(x,v,label='Implicit')
+    
+def eulerphasesymp(x0,v0,n,h,fig):
+    t = np.zeros(n)
+    x = np.zeros(n)
+    v = np.zeros(n)
+    x[0] = x0
+    v[0] = v0
+    for i in range (n-1):
+        t[i+1]=h*(i+1)
+        x[i+1]=x[i]+h*v[i]
+        v[i+1]=v[i]-h*x[i+1]
+    fig=plt.plot(x,v,label='Symplectic')
+    
+def eulerenergysymp(x0,v0,n,h,fig):
+    t = np.zeros(n)
+    x = np.zeros(n)
+    v = np.zeros(n)
+    x[0] = x0
+    v[0] = v0
+    for i in range (n-1):
+        t[i+1]=h*(i+1)
+        x[i+1]=x[i]+h*v[i]
+        v[i+1]=v[i]-h*x[i+1]
+    E = x*x+v*v
+    fig=plt.plot(t,E,label='Symplectic')
